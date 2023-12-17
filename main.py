439063e0904931cafb661b02d423f7e0ce5f2052
Yusufova <yusufova.odina11@gmail.com>
2023-12-17 16:20:32 +0500

    finished: 16:16

---
+++ main.py
@@ -0,0 +1,29 @@
+ #1
+# l = [1,2,4,6,7,8,9,10]
+# def missed_num():
+#     for x in range(1, 11):
+#         if x not in l:
+#             return x
+# missed_num()
+
+#2
+# def cars():
+#
+#  i = int(input("Enter any number:"))
+#  y = i // 5
+#  print(y)
+#
+# cars()
+
+#3
+o = input("Enter percent:")
+f = [6, 8, 10]
+
+def percent(o,f):
+   result = []
+   for x in f:
+       result.append(o / 100 + x)
+     return f
+
+
+
